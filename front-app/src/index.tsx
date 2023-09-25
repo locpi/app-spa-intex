dd33985b3c1a39da8eda3d1e600739c68ff2cbcc
Loïc Pinçon <loic.pincon.ext@evollis.com>
2023-09-25 18:06:13 +0200

    second commit

---
+++ front-app/src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './App.scss';
+
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+<p>Hello</p>
+    </React.StrictMode>
+);
