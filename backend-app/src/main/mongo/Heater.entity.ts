b041632ca781eb0cfbdf99813502a58baf993252
Loïc Pinçon <loic.pincon29@gmail.com>
2023-12-31 15:19:32 +0100

    update entities

--- backend-app/src/main/mongo/Heater.entity.ts
+++ backend-app/src/main/mongo/Heater.entity.ts
@@ -1,26 +1,31 @@
 import { MongoEntity, MongoRepository } from "~/main/mongo/tools/MongoRepository";
 import { CommandState } from "~/main/model/CommandState";
+import { v4 as uuidv4 } from 'uuid';
 
 export class HeaterEntity implements MongoEntity {
 
+  private readonly id: string;
   private readonly date: Date;
   private readonly status: CommandState;
 
 
   constructor(date: Date, status: CommandState) {
+    this.id = uuidv4();
     this.date = date;
     this.status = status;
   }
 
   _getId(): any {
     return {
-      date: this.date,
-      status: this.status
+      id: this.id
     }
   }
 
   getOtherField(): any {
-
+    return {
+      date: this.date,
+      status: this.status
+    }
   }
 
 }
--- backend-app/src/main/mongo/Power.entity.ts
+++ backend-app/src/main/mongo/Power.entity.ts
@@ -1,28 +1,32 @@
-import {MongoEntity, MongoRepository} from "~/main/mongo/tools/MongoRepository";
-import {CommandState} from "~/main/model/CommandState";
+import { MongoEntity, MongoRepository } from "~/main/mongo/tools/MongoRepository";
+import { CommandState } from "~/main/model/CommandState";
+import { v4 as uuidv4 } from 'uuid';
 
 export class PowerEntity implements MongoEntity {
 
+  private readonly id: string;
   private readonly date: Date;
   private readonly status: CommandState;
 
 
   constructor(date: Date, status: CommandState) {
+    this.id = uuidv4();
     this.date = date;
     this.status = status;
   }
 
   _getId(): any {
     return {
-      date:this.date,
-      status: this.status
+      id: this.id
     }
   }
 
   getOtherField(): any {
-
+    return {
+      date: this.date,
+      status: this.status
+    }
   }
-
 }
 
 export class PowerEntityRepository extends MongoRepository<PowerEntity> {
