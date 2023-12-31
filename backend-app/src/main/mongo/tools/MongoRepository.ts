cee1e4e8620c2a5fdef07c0010cfc64fc9d3c7be
Loïc Pinçon <loic.pincon29@gmail.com>
2023-12-31 17:01:31 +0100

    fix bug save spa information

--- backend-app/src/main/mongo/tools/MongoRepository.ts
+++ backend-app/src/main/mongo/tools/MongoRepository.ts
@@ -65,7 +65,8 @@ export abstract class MongoRepository<E extends MongoEntity> extends MongoConnec
   public async findFirstWithParamsSort(param: any, sort: any): Promise<E> {
     const client = await super.getClient();
     const collection = await client.db().collection(this.collection);
-    return await collection.find(param, { sort: sort }).toArray()[0];
+    const promise = await collection.find(param, { sort: sort }).toArray();
+    return promise.length > 0 ? promise[0] : null;
   }
 
 
--- backend-app/src/main/mqtt/SpaInformationsTopic.ts
+++ backend-app/src/main/mqtt/SpaInformationsTopic.ts
@@ -1,49 +1,58 @@
 import { SpaInformationEntity, SpaInformationEntityRepository } from './../mongo/SpaInformation.entity';
 import { AbstractMqttService } from "./AbstractMqttService";
 
-export class SpaInformationsTopic extends AbstractMqttService{
-    private readonly spaInformationEntityRepository: SpaInformationEntityRepository = new SpaInformationEntityRepository();
+export class SpaInformationsTopic extends AbstractMqttService {
+  private readonly spaInformationEntityRepository: SpaInformationEntityRepository = new SpaInformationEntityRepository();
 
-    constructor() {
-      super();
-  
-      super.subscribeTo("wifi/state", ((message: string) => {
-        const newLine = new SpaInformationEntity();
-        newLine.spaStatus = message;
-        this.saveNewLineOfInformations(newLine);
-      }))
-      super.subscribeTo("wifi/ip", ((message: string) => {
-        const newLine = new SpaInformationEntity();
-        newLine.ip = message;
-        this.saveNewLineOfInformations(newLine);
-      }))
-      super.subscribeTo("wifi/rssi", ((message: string) => {
-        const newLine = new SpaInformationEntity();
-        newLine.rssi = message;
-        this.saveNewLineOfInformations(newLine);
-      }))
-      super.subscribeTo("wifi/version", ((message: string) => {
-        const newLine = new SpaInformationEntity();
-        newLine.version = message;
-        this.saveNewLineOfInformations(newLine);
-      }))
-      super.subscribeTo("pool/model", ((message: string) => {
-        const newLine = new SpaInformationEntity();
-        newLine.model = message;
-        this.saveNewLineOfInformations(newLine);
-      }))
-    }
+  constructor() {
+    super();
 
-  
-    public getLastLineOfInformations():Promise<SpaInformationEntity>{
-      return this.spaInformationEntityRepository.findFirstWithParamsSort({}, {date: -1});
-    }
+    super.subscribeTo("wifi/state", ((message: string) => {
+      const newLine = new SpaInformationEntity();
+      newLine.spaStatus = message;
+      this.saveNewLineOfInformations(newLine);
+    }))
+    super.subscribeTo("wifi/ip", ((message: string) => {
+      const newLine = new SpaInformationEntity();
+      newLine.ip = message;
+      this.saveNewLineOfInformations(newLine);
+    }))
+    super.subscribeTo("wifi/rssi", ((message: string) => {
+      const newLine = new SpaInformationEntity();
+      newLine.rssi = message;
+      this.saveNewLineOfInformations(newLine);
+    }))
+    super.subscribeTo("wifi/version", ((message: string) => {
+      const newLine = new SpaInformationEntity();
+      newLine.version = message;
+      this.saveNewLineOfInformations(newLine);
+    }))
+    super.subscribeTo("pool/model", ((message: string) => {
+      const newLine = new SpaInformationEntity();
+      newLine.model = message;
+      this.saveNewLineOfInformations(newLine);
+    }))
+  }
 
-    public saveNewLineOfInformations(newest:SpaInformationEntity):void{
-      this.getLastLineOfInformations().then(old=>{
-        const merged = Object.assign(new SpaInformationEntity(), old, newest) as  SpaInformationEntity ;
+
+  public getLastLineOfInformations(): Promise<SpaInformationEntity> {
+    return this.spaInformationEntityRepository.findFirstWithParamsSort({}, { date: -1 });
+  }
+
+  public saveNewLineOfInformations(newest: SpaInformationEntity): void {
+    this.getLastLineOfInformations().then(old => {
+      if (old) {
+        const id = old.id;
+        const merged = Object.assign(new SpaInformationEntity(), old, newest) as SpaInformationEntity;
+        merged.id = id;
         this.spaInformationEntityRepository.save(merged);
-      })
-    }
+
+      } else {
+        this.spaInformationEntityRepository.save(newest);
+
+      }
+
+    })
+  }
 
 }
